c9cc3d712314787517e4a03bf92c6d6d8a03e740
omandac <34442679+omandac@users.noreply.github.com>
2018-01-10 18:37:22 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
 	<title></title>
 </head>
 <body>
-<img src = "1.png" width = "100px" height = "20%" >
+<img src = "1.png" width = "25%" height = "100px" >
+	<img src = "2.png" width = "25%" height = "100px" >
+	<img src = "3.png" width = "25%" height = "100px" >
+	<img src = "4.png" width = "25%" height = "100px" >
+	<img src = "5.png" width = "25%" height = "100px" >
+	<img src = "6.png" width = "25%" height = "100px" >
 </body>
 </html>
